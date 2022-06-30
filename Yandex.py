8ac466eedd4e6c48b0ca7f000028f1291d7b863e
Zhebel Alika <rezedawasser@gmail.com>
2022-06-30 21:31:19 +0300

    Zadanie - 2

---
+++ Yandex.py
@@ -0,0 +1,32 @@
+import requests
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+            return {
+                'Content-Type': 'application/json',
+                'Authorization': 'OAuth {}'.format(self.token)
+            }
+       
+    def _get_upload_link(self, file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def upload(self, file_path: str):
+        href = self._get_upload_link(file_path=file_path).get("href", "")
+        response = requests.put(href, data=open(path_to_file, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+
+
+if __name__ == '__main__':
+    path_to_file = input('Введите путь к файлу на компьютере:\n')
+    token = input('Введите свой Токен:\n')
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file) 
